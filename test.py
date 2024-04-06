f4354ae9205366199ecaa2acbb6c8536f453bcaa
Mike <garzarobm@gmail.com>
2024-04-06 02:32:54 -0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,5 @@
+    def hello_world():
+    # TODO(alstr): Come up with a more imaginative greeting
+    #  Everyone uses hello world and it's boring.
+    #  labels: enhancement, help wanted
+    print('Hello world!')
